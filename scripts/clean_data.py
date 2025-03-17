86869edadfa252a414106be99d9bf3806c665f14
ruseny <rusen138@gmail.com>
2025-03-17 10:58:19 +0100

    already cleaned files considered

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -15,8 +15,29 @@ from bs4 import MarkupResemblesLocatorWarning
 warnings.filterwarnings("ignore", category=MarkupResemblesLocatorWarning, module='src.data_cleaning')
 
 def main():
-    # get the list of files in the directory
+
+    # get the list of files in the processed directory
     list_files = [f for f in os.listdir("../data/processed") if os.path.isfile(os.path.join("../data/processed", f))]
+
+    # check the processed directory, stop if empty
+    if len(list_files) == 0:
+        print("The directory ../data/processed is empty. Please run the script get_data.py first.")
+        return
+    
+    # check if the cleaned directory exists, if so, get the list of files in it
+    if os.path.exists("../data/cleaned"):
+        list_files_cleaned = [f for f in os.listdir("../data/cleaned") if os.path.isfile(os.path.join("../data/cleaned", f))]
+        # if it is not empty, remove the existing file names from the list of files to be cleaned, based on timestamps
+        if len(list_files_cleaned) > 0:
+            alread_cleaned_timestamps = [name[-19:-4] for name in list_files_cleaned]
+            list_files = [f for f in list_files if f[-19:-4] not in alread_cleaned_timestamps]
+            # if there is no more files to clean, stop
+            if len(list_files) == 0:
+                print("All files have been cleaned already.")
+                return
+    # if the cleaned directory does not exist, create it
+    else:
+        os.mkdir("../data/cleaned")
 
     # prepare paths for trending and adjacent
     paths_trending = [os.path.join("../data/processed", f) for f in list_files if f[:8] == "trending"]
@@ -25,13 +46,12 @@ def main():
     paths_adjacent.sort()
 
     # output df names will have the timestamp of the corresponding trending statuses
-    df_names = ["df" + f[17:33] for f in list_files if f[:8] == "trending"]
+    df_names = ["df_" + f[-19:-4] for f in list_files if f[:8] == "trending"]
 
-    # match trending datasets with correponding adjacent datasets
+    # match trending datasets with correponding adjacent datasets, based on timestamps
     match_dfs = {}
     for path in paths_trending:
-        i = paths_trending.index(path)
-        match_dfs[path] = paths_adjacent[i*4 : (i+1)*4]
+        match_dfs[path] = [p for p in paths_adjacent if path[-19:-4] in p]
     
     # collect processed dfs in a dictionary
     dict_dfs = {}
